e733827a262b9848ef4b9ce24cf7e06646e03cc2
Barbie-6 <83456139+Barbie-6@users.noreply.github.com>
2022-03-16 17:56:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ left_wrist_y = '';
 right_wrist_x = '';
 right_wrist_y = '';
 
+leftWrist_score = '';
+rightWrist_score = '';
+
 function preload() {
     peter_pan_music = loadSound("music2.mp3");
     harry_potter_music = loadSound("music.mp3");
@@ -22,6 +25,33 @@ function setup() {
 }
 function draw() {
     image(video, 0, 0, 500, 400);
+
+    peter_pan_status = peter_pan_music.isPlaying();
+    harry_potter_status = harry_potter_music.isPlaying();
+
+    fill("blue");
+    stroke("blue");
+    if(leftWrist_score > 0.2){
+        circle(left_wrist_x, left_wrist_y, 20);
+        if(peter_pan_status == false){
+            peter_pan_music.play();
+            document.getElementById("status").innerHTML = "Playing peter pan music";
+            harry_potter_music.stop();
+        }
+        
+    }
+
+    if(rightWrist_score > 0.2){
+        circle(right_wrist_x, right_wrist_y, 20);
+        if(harry_potter_status == false){
+            harry_potter_music.play();
+            document.getElementById("status").innerHTML = "Playing Harry potter music";
+            peter_pan_music.stop();
+        }
+        
+    }
+
+
 }
 function modelLoaded() {
     console.log("model is loaded!!");
@@ -35,6 +65,9 @@ if(results.length > 0){
 
     right_wrist_x = results[0].pose.rightWrist.x;
     right_wrist_x = results[0].pose.rightWrist.y;
+
+    leftWrist_score = results[0].pose.keypoints[9].score;
+    rightWrist_score = results[0].pose.keypoints[10].score;
 
     console.log("Left wrist x- " + left_wrist_x + " left wrist y- " + left_wrist_y);
     console.log("right wrist x- " + right_wrist_x + " right wrist y- " + right_wrist_y);
